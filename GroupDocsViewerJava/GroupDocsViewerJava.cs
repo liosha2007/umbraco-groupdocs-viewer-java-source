77fefb7dc9fc8010a30da3d021fb9d224fba9cfa
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2013-11-22 15:37:15 +0200

    Initial commit: Empty project

---
+++ GroupDocsViewerJava/GroupDocsViewerJava.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Groupdocs.GroupDocsViewerJava
+{
+    public class GroupDocsViewerJava : System.Web.UI.UserControl
+    {
+        public String test { get; set; }
+    }
+}
